10c99d19d8bf460b504bb9ecefff339672e3b615
alexmortelsison <alexmortelsison@gmail.com>
2024-03-31 16:48:47 +0900

    Initial Commit

---
+++ Japan City List/Japan_City_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Japan_City_ListApp.swift
+//  Japan City List
+//
+//  Created by Alex Sison on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Japan_City_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
